bf719fb1fa3a2a7f94c24216617c189a3efa8c5e
Becca King <becca.king3232@gmail.com>
2020-07-07 21:26:07 -0400

    Added Native Plant Finder data

--- gardening-app/app.js
+++ gardening-app/app.js
@@ -1,1 +1,725 @@
-console.log('app')
+const plants = [
+  {
+    name: ['oak'],
+    species: 'Quercus',
+    genus: 'Fagales',
+    count: 477,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/2804'
+  },
+  {
+    name: ['beach plum', 'cherry', 'chokecherry', 'peach', 'plum', 'sweet cherry', 'wild plum', 'almond'],
+    species: 'Prunus',
+    genus: 'Rosales',
+    count: 381,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/2732'
+  },
+  {
+    name: ['willow'],
+    species: 'Salix',
+    genus: 'Salicales',
+    count: 328,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/2908'
+  },
+  {
+    name: ['birch'],
+    species: 'Betula',
+    genus: 'Fagales',
+    count: 317,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/398'
+  },
+  {
+    name: ['aspen', 'cottonwood', 'poplar'],
+    species: 'Populus',
+    genus: 'Salicales',
+    count: 287,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/2700'
+  },
+  {
+    name: ['maple', 'boxelder'],
+    species: 'Acer',
+    genus: 'Sapindales',
+    count: 273,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/16'
+  },
+  {
+    name: ['crabapple', 'apple'],
+    species: 'Malus',
+    genus: 'Rosales',
+    count: 256,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/2070'
+  },
+  {
+    name: ['hickory', 'pecan', 'pignut', 'bitternut'],
+    species: 'Carya',
+    genus: 'Juglandales',
+    count: 231,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/605'
+  },
+  {
+    name: ['cranberry', 'blueberry'],
+    species: 'Vaccinium',
+    genus: 'Ericales',
+    count: 223,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/3423'
+  },
+  {
+    name: ['alder'],
+    species: 'Alnus',
+    genus: 'Fagales',
+    count: 188,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/112'
+  },
+  {
+    name: ['elm'],
+    species: 'Ulmus',
+    genus: 'Urticales',
+    count: 185,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/3409'
+  },
+  {
+    name: ['hawthorn'],
+    species: 'Crataegus',
+    genus: 'Rosales',
+    count: 154,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/890'
+  },
+  {
+    name: ['basswood', 'linden'],
+    species: 'Tilia',
+    genus: 'Malvales',
+    count: 151,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/3301'
+  },
+  {
+    name: ['blackberry', 'dewberry', 'raspberry', 'thimbleberry'],
+    species: 'Rubus',
+    genus: 'Rosales',
+    count: 146,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/2881'
+  },
+  {
+    name: ['butternut'],
+    species: 'Juglans',
+    genus: 'Juglandales',
+    count: 136,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/1780'
+  },
+  {
+    name: ['ash'],
+    species: 'Fraxinus',
+    genus: 'Scrophulariales',
+    count: 135,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/1359'
+  },
+  {
+    name: ['beech'],
+    species: 'Fagus',
+    genus: 'Fagales',
+    count: 126,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/1310'
+  },
+  {
+    name: ['chestnut'],
+    species: 'Castanea',
+    genus: 'Fagales',
+    count: 122,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/615'
+  },
+  {
+    name: ['goldenrod'],
+    species: 'Solidago',
+    genus: 'Asterales',
+    count: 122,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/3081'
+  },
+  {
+    name: ['filbert', 'hazel', 'hazelnut'],
+    species: 'Corylus',
+    genus: 'Fagales',
+    count: 118,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/870'
+  },
+  {
+    name: ['rose', 'sweetbriar'],
+    species: 'Rosa',
+    genus: 'Rosales',
+    count: 114,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/2872'
+  },
+  {
+    name: ['dogwood', 'bunchberry'],
+    species: 'Cornus',
+    genus: 'Cornales',
+    count: 111,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/863'
+  },
+  {
+    name: ['serviceberry', 'juneberry', 'shadbush'],
+    species: 'Amelanchier',
+    genus: 'Rosales',
+    count: 105,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/136'
+  },
+  {
+    name: ['arrowwood', 'viburnum', 'wild rasin'],
+    species: 'Viburnum',
+    genus: 'Dipsacales',
+    count: 101,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/3449'
+  },
+  {
+    name: ['tupelo', 'ironwood'],
+    species: 'Ostrya',
+    genus: 'Fagales',
+    count: 87,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/2418'
+  },
+  {
+    name: ['sunflower'],
+    species: 'Helianthus',
+    genus: 'Asterales',
+    count: 76,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/1561'
+  },
+  {
+    name: ['American hornbeam'],
+    species: 'Carpinus',
+    genus: 'Fagales',
+    count: 74,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/600'
+  },
+  {
+    name: ['grape'],
+    species: 'Vitis',
+    genus: 'Rhamnales',
+    count: 71,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/3458'
+  },
+  {
+    name: ['strawberry'],
+    species: 'Fragaria',
+    genus: 'Rosales',
+    count: 71,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/1353'
+  },
+  {
+    name: ['witchhazel'],
+    species: 'Hamamelis',
+    genus: 'Hamamelidales',
+    count: 69,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/1521'
+  },
+  {
+    name: ['meadowsweet', 'spirea'],
+    species: 'Spiraea',
+    genus: 'Rosales',
+    count: 68,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/3115'
+  },
+  {
+    name: ['locust', 'black locust', 'rose acacia'],
+    species: 'Robinia',
+    genus: 'Fabales',
+    count: 61,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/2861'
+  },
+  {
+    name: ['mountain ash'],
+    species: 'Sorbus',
+    genus: 'Rosales',
+    count: 59,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/3087'
+  },
+  {
+    name: ['sumac'],
+    species: 'Rhus',
+    genus: 'Sapindales',
+    count: 54,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/2851'
+  },
+  {
+    name: ['honeylocust','gleditsia'],
+    species: 'Gleditsia',
+    genus: 'Fabales',
+    count: 47,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/1435'
+  },
+  {
+    name: ['sycamore'],
+    species: 'Platanus',
+    genus: 'Hamamelidales',
+    count: 42,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/2636'
+  },
+  {
+    name: ['holly', 'inkberry', 'winterberry'],
+    species: 'Ilex',
+    genus: 'Celastrales',
+    count: 39,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/1722'
+  },
+  {
+    name: ['huckleberry', 'dangleberry'],
+    species: 'Gaylussacia',
+    genus: 'Ericales',
+    count: 38,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/1400'
+  },
+  {
+    name: ['horsechestnut', 'buckeye'],
+    species: 'Aesculus',
+    genus: 'Sapindales',
+    count: 37,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/62'
+  },
+  {
+    name: ['New Jersey tea'],
+    species: 'Ceanothus',
+    genus: 'Rhamnales',
+    count: 37,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/635'
+  },
+  {
+    name: ['ninebark'],
+    species: 'Physocarpus',
+    genus: 'Rosales',
+    count: 34,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/2583'
+  },
+  {
+    name: ['blackgum', 'sourgum', 'tupelo'],
+    species: 'Nyssa',
+    genus: 'Cornales',
+    count: 33,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/2344'
+  },
+  {
+    name: ['elderberry'],
+    species: 'Sambucus',
+    genus: 'Dipsacales',
+    count: 33,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/2917'
+  },
+  {
+    name: ['joe-pye weed', 'thoroughwort', 'dogfennel', 'common boneset'],
+    species: 'Eupatorium',
+    genus: 'Asterales',
+    count: 32,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/1284'
+  },
+  {
+    name: ['sassafras'],
+    species: 'Sassafras',
+    genus: 'Laurales',
+    count: 31,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/2939'
+  },
+  {
+    name: ['violet'],
+    species: 'Viola',
+    genus: 'Violales',
+    count: 29,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/3455'
+  },
+  {
+    name: ['virginia creeper', 'woodbine', 'Boston Ivy'],
+    species: 'Parthenocissus',
+    genus: 'Rhamnales',
+    count: 29,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/2473'
+  },
+  {
+    name: ['juniper', 'red cedar'],
+    species: 'Juniperus',
+    genus: 'Pinales',
+    count: 28,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/1782'
+  },
+  {
+    name: ['bulb panic grass', 'maidencane', 'winged panicgrass', 'panicgrass', 'switchgrass'],
+    species: 'Panicum',
+    genus: 'Cyperales',
+    count: 27,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/2453'
+  },
+  {
+    name: ['deer vetch', 'trefoil'],
+    species: 'Lotus',
+    genus: 'Fabales',
+    count: 26,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/2004'
+  },
+  {
+    name: ['honeysuckle'],
+    species: 'Lonicera',
+    genus: 'Dispacales',
+    count: 26,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/1995'
+  },
+  {
+    name: ['buttonbush'],
+    species: 'Cephalanthus',
+    genus: 'Rubiales',
+    count: 25,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/655'
+  },
+  {
+    name: ['false indigo', 'indigobush', 'leadplant'],
+    species: 'Amorpha',
+    genus: 'Fabales',
+    count: 25,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/144'
+  },
+  {
+    name: ['water parsnip'],
+    species: 'Sium',
+    genus: 'Apiales',
+    count: 24,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/3069'
+  },
+  {
+    name: ['browneyed susan', 'blackeyed susan', 'coneflower'],
+    species: 'Rudbeckia',
+    genus: 'Asterales',
+    count: 23,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/2882'
+  },
+  {
+    name: ['hibiscus', 'rosemallow'],
+    species: 'Hibiscus',
+    genus: 'Malvales',
+    count: 23,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/1622'
+  },
+  {
+    name: ['St. Johns wort'],
+    species: 'Hypericum',
+    genus: 'Theales',
+    count: 23,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/1707'
+  },
+  {
+    name: ['ironweed'],
+    species: 'Vernonia',
+    genus: 'Asterales',
+    count: 22,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/3445'
+  },
+  {
+    name: ['flax'],
+    species: 'Linum',
+    genus: 'Linales',
+    count: 20,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/1958'
+  },
+  {
+    name: ['snowberry', 'buckbush'],
+    species: 'Symphoricarpos',
+    genus: 'Dipsacales',
+    count: 20,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/3194'
+  },
+  {
+    name: ['willowherb'],
+    species: 'Epilobium',
+    genus: 'Myrtales',
+    count: 20,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/1218'
+  },
+  {
+    name: ['fleabane'],
+    species: 'Erigeron',
+    genus: 'Asterales',
+    count: 19,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/1241'
+  },
+  {
+    name: ['greenbriar'],
+    species: 'Smilax',
+    genus: 'Liliales',
+    count: 19,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/3074'
+  },
+  {
+    name: ['magnolia', 'sweetbay'],
+    species: 'Magnolia',
+    genus: 'Magnoliales',
+    count: 18,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/2056'
+  },
+  {
+    name: ['tulip poplar', 'tuliptree'],
+    species: 'Liriodendron',
+    genus: 'Magnoliales',
+    count: 18,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/1964'
+  },
+  {
+    name: ['crownbeard', 'wingstem'],
+    species: 'Verbesina',
+    genus: 'Asterales',
+    count: 17,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/3443'
+  },
+  {
+    name: ['dogbane'],
+    species: 'Apocynum',
+    genus: 'Gentianales',
+    count: 17,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/226'
+  },
+  {
+    name: ['indigo', 'wild indigo'],
+    species: 'Baptisia',
+    genus: 'Fabales',
+    count: 17,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/356'
+  },
+  {
+    name: ['indigo', 'wild indigo'],
+    species: 'Baptisia',
+    genus: 'Fabales',
+    count: 17,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/356'
+  },
+  {
+    name: ['whitetop'],
+    species: 'Doellingeria',
+    genus: 'Asterales',
+    count: 16,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/1103'
+  },
+  {
+    name: ['evening primrose'],
+    species: 'Oenothera',
+    genus: 'Myrtales',
+    count: 15,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/2359'
+  },
+  {
+    name: ['feverfew', 'quinine'],
+    species: 'Parthenium',
+    genus: 'Asterales',
+    count: 15,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/2472'
+  },
+  {
+    name: ['purslane', 'hogweed', 'chickenweed', 'rose moss'],
+    species: 'Portulaca',
+    genus: 'Caryophyllales',
+    count: 15,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/2704'
+  },
+  {
+    name: ['bluestem', 'broomsedge bluestem'],
+    species: 'Andropogon',
+    genus: 'Cyperales',
+    count: 14,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/170'
+  },
+  {
+    name: ['brackenfern', 'bracken'],
+    species: 'Pteridium',
+    genus: 'Polypodiales',
+    count: 14,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/2771'
+  },
+  {
+    name: ['buffaloberry'],
+    species: 'Shepherdia',
+    genus: 'Rhamnales',
+    count: 14,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/3039'
+  },
+  {
+    name: ['butterweed', 'ragwort'],
+    species: 'Senecio',
+    genus: 'Asterales',
+    count: 14,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/3022'
+  },
+  {
+    name: ['cattail'],
+    species: 'Typha',
+    genus: 'Typhales',
+    count: 14,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/3407'
+  },
+  {
+    name: ['meadow rue'],
+    species: 'Thalictrum',
+    genus: 'Ranunculales',
+    count: 14,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/3265'
+  },
+  {
+    name: ['wahoo', 'spindletree', 'burstingheart', 'wintercreeper', 'strawberrybush'],
+    species: 'Euonymus',
+    genus: 'Celastrales',
+    count: 14,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/1283'
+  },
+  {
+    name: ['yarrow', 'sneezeweed'],
+    species: 'Achillea',
+    genus: 'Asterales',
+    count: 14,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/17'
+  },
+  {
+    name: ['brome'],
+    species: 'Bromus',
+    genus: 'Cyperales',
+    count: 13,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/473'
+  },
+  {
+    name: ['cordgrass'],
+    species: 'Spartina',
+    genus: 'Cyperales',
+    count: 13,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/3093'
+  },
+  {
+    name: ['iris'],
+    species: 'Iris',
+    genus: 'Liliales',
+    count: 13,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/1738'
+  },
+  {
+    name: ['pawpaw'],
+    species: 'Asimina',
+    genus: 'Magnoliales',
+    count: 13,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/297'
+  },
+  {
+    name: ['touch-me-not', 'impatiens'],
+    species: 'Impatiens',
+    genus: 'Geraniales',
+    count: 13,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/1726'
+  },
+  {
+    name: ['aster', 'oldfield aster'],
+    species: 'Symphyotrichum',
+    genus: 'Asterales',
+    count: 12,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/3195'
+  },
+  {
+    name: ['groundcherry'],
+    species: 'Physalis',
+    genus: 'Solanales',
+    count: 12,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/2581'
+  },
+  {
+    name: ['leatherleaf'],
+    species: 'Chamaedaphne',
+    genus: 'Ericales',
+    count: 12,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/686'
+  },
+  {
+    name: ['milkweed', 'butterfly weed'],
+    species: 'Asclepias',
+    genus: 'Gentianales',
+    count: 12,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/296'
+  },
+  {
+    name: ['spicebush'],
+    species: 'Lindera',
+    genus: 'Laurales',
+    count: 12,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/1953'
+  },
+  {
+    name: ['beebalm', 'wild bergamot'],
+    species: 'Monarda',
+    genus: 'Lamiales',
+    count: 11,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/2206'
+  },
+  {
+    name: ['columbine'],
+    species: 'Aquilegia',
+    genus: 'Ranunculales',
+    count: 11,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/231'
+  },
+  {
+    name: ['managrass', 'fowl managrass'],
+    species: 'Glyceria',
+    genus: 'Cyperales',
+    count: 11,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/1446'
+  },
+  {
+    name: ['verbena', 'vervain'],
+    species: 'Verbena',
+    genus: 'Lamiales',
+    count: 11,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/3442'
+  },
+  {
+    name: ['woolgrass', 'bulrush'],
+    species: 'Scirpus',
+    genus: 'Cyperales',
+    count: 11,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/2988'
+  },
+  {
+    name: ['beardtongue', 'penstemon'],
+    species: 'Penstemon',
+    genus: 'Scrophulariales',
+    count: 10,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/2501'
+  },
+  {
+    name: ['cinquefoil'],
+    species: 'Potentilla',
+    genus: 'Rosales',
+    count: 10,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/2708'
+  },
+  {
+    name: ['coral bristlegrass'],
+    species: 'Setaria',
+    genus: 'Cyperales',
+    count: 10,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/3035'
+  },
+  {
+    name: ['lily', 'tiger lily'],
+    species: 'Lilium',
+    genus: 'Liliales',
+    count: 10,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/1943'
+  },
+  {
+    name: ['milkvetch'],
+    species: 'Astragalus',
+    genus: 'Fabales',
+    count: 10,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/308'
+  },
+  {
+    name: ['sagebrush', 'wormwood'],
+    species: 'Artemisia',
+    genus: 'Asterales',
+    count: 10,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/282'
+  },
+  {
+    name: ['vetch'],
+    species: 'Vicia',
+    genus: 'Fabales',
+    count: 10,
+    url: 'https://www.nwf.org/NativePlantFinder/Plants/3450'
+  }
+]
+
+console.log(plants)
