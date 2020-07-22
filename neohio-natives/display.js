949994bd06bca31690c7e64931faa9737d641c52
Becca King <becca.king3232@gmail.com>
2020-07-22 11:53:29 -0400

    Finished adding photos, added display plan notes

---
+++ neohio-natives/display.js
@@ -0,0 +1,5 @@
+//Create photo grid of plants, size proportionate to the number of dependent species
+//Add the species name as a class
+//Pull in random order
+//Make each photo a div w/the photo as the background url so you can set background position where necessary
+//Intersperse divs with quotes from Dr. Tallamy about the importance of native species, facts from his books, plugs for his books, esp newest- Nature's Best Hope
