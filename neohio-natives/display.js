3c0c6774b93b48cf902bf29176aab3f869884535
Becca King <becca.king3232@gmail.com>
2020-07-27 14:55:25 -0400

    Core layout

--- neohio-natives/display.js
+++ neohio-natives/display.js
@@ -3,6 +3,8 @@
 //Intersperse divs with quotes from Dr. Tallamy about the importance of native species, facts from his books, plugs for his books, esp newest- Nature's Best Hope
 
 let container = document.getElementsByClassName('photo-container');
+let midcontainer = document.getElementsByClassName('mid-photo-container');
+let smallcontainer = document.getElementsByClassName('small-photo-container');
 
 
 for (i=0;i<plants.length;i++){
@@ -19,18 +21,74 @@ for (i=0;i<plants.length;i++){
   plant.classList.add(plants[i].species)
 
 
-  container[0].append(plant)
 
   plant.style.backgroundImage = "url('./images/" + plants[i].species +".jpeg')"
   if(plants[i].count < 30){
-    plant.style.width = plants[i].count*20 + 'px';
-    plant.style.height = plants[i].count*20 + 'px';
-  } else if (30 <= plants[i].count < 100) {
-    plant.style.width = plants[i].count*5 + 'px';
-    plant.style.height = plants[i].count*5 + 'px';
+    plant.style.width = plants[i].count + 'px';
+    plant.style.height = plants[i].count + 'px';
+    plant.classList.add('small')
+    smallcontainer[0].append(plant)
+  } else if (30 <= plants[i].count && plants[i].count < 100) {
+    plant.style.width = plants[i].count + 'px';
+    plant.style.height = plants[i].count + 'px';
+    plant.classList.add('mid')
+    midcontainer[0].append(plant)
   } else {
     plant.style.width = plants[i].count + 'px';
     plant.style.height = plants[i].count + 'px';
+    plant.classList.add('large')
+    container[0].append(plant)
   }
 
 }
+
+var smalls = document.getElementsByClassName('small')
+var enlarge = document.getElementsByClassName('click')
+
+enlarge[0].addEventListener('click', function(){
+  if(smallcontainer[0].classList.contains('grid')){
+    for (i=0;i<smalls.length;i++){
+      smalls[i].classList.remove('grid-small')
+    }
+    smallcontainer[0].classList.remove('grid')
+  } else {
+    for (i=0;i<smalls.length;i++){
+      smalls[i].classList.add('grid-small')
+    }
+    smallcontainer[0].classList.add('grid')
+  }
+})
+
+var mids = document.getElementsByClassName('mid')
+var enlargemid = document.getElementsByClassName('click-mid')
+
+enlargemid[0].addEventListener('click', function(){
+  if(midcontainer[0].classList.contains('grid')){
+    for (i=0;i<mids.length;i++){
+      mids[i].classList.remove('grid-mid')
+    }
+    midcontainer[0].classList.remove('grid')
+  } else {
+    for (i=0;i<mids.length;i++){
+      mids[i].classList.add('grid-mid')
+    }
+    midcontainer[0].classList.add('grid')
+  }
+})
+
+var larges = document.getElementsByClassName('large')
+var enlargelarge = document.getElementsByClassName('click-large')
+
+enlargelarge[0].addEventListener('click', function(){
+  if(container[0].classList.contains('grid')){
+    for (i=0;i<larges.length;i++){
+      larges[i].classList.remove('grid-large')
+    }
+    container[0].classList.remove('grid')
+  } else {
+    for (i=0;i<larges.length;i++){
+      larges[i].classList.add('grid-large')
+    }
+    container[0].classList.add('grid')
+  }
+})
