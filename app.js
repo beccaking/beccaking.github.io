dbf55c628a89f42d8250ca0a7541d49afc4a6831
beccaking <rebecca.lynne.king@gmail.com>
2021-12-10 22:17:23 -0500

    Updates

--- app.js
+++ app.js
@@ -1,3 +1,43 @@
 $(function(){
-  $('.backdrop').fadeOut();
+  let allMedia = document.querySelectorAll('.media div')
+
+  for(i=0;i<allMedia.length;i++){
+    allMedia[i].setAttribute('id', i)
+  }
+
+  $('.arrow.left').on('click keypress', function(event){
+    if(event.keyCode == 13 || $(this).data('clicked', true)){
+      let activeCard = document.getElementsByClassName('active')[0]
+      let activeCardId = activeCard.getAttribute('id')
+      let lastCard = document.getElementById((allMedia.length - 1).toString())
+      let firstCard = document.getElementById('0')
+
+      activeCard.classList.remove('active')
+
+      if(activeCardId == 0){
+        lastCard.classList.add('active')
+      } else {
+        let prevCard = document.getElementById(parseInt(activeCardId) - 1)
+        prevCard.classList.add('active')
+      }
+    }
+  })
+
+  $('.arrow.right').on('click keypress', function(event){
+    if(event.keyCode == 13 || $(this).data('clicked', true)){
+      let activeCard = document.getElementsByClassName('active')[0]
+      let activeCardId = activeCard.getAttribute('id')
+      let lastCard = document.getElementById((allMedia.length - 1).toString())
+      let firstCard = document.getElementById('0')
+
+      activeCard.classList.remove('active')
+      
+      if(activeCardId == lastCard.getAttribute('id')){
+        firstCard.classList.add('active')
+      } else {
+        let nextCard = document.getElementById((parseInt(activeCardId) + 1))
+        nextCard.classList.add('active')
+      }
+    }
+  })
 })
